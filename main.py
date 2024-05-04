3c2c59e047ea537abe38558a802e449c39787ea4
Tail <63604688+ThePror9nsOut@users.noreply.github.com>
2024-05-04 17:51:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ run = True
 distance_divider = 30
 overall_divider = 40
 friction_divider = 1000
-bounciness_divider = 50
+bounciness_divider = 100
 
 gravity = 0.005
 
@@ -16,11 +16,7 @@ gravity = 0.005
 char = [250,250]
 dx, dy = 0, 0
 
-last_time = time.time()
-
 while run:
-    delta = time.time() - last_time
-    last_time = time.time()
 
     win.fill((0,0,0))
 
@@ -29,7 +25,7 @@ while run:
     mouse = pg.mouse.get_pos()
 
     if pg.mouse.get_pressed()[0]:
-        pg.draw.line(win,(255,255,255), char, mouse)
+        pg.draw.line(win, (255,255,255), char, mouse)
 
         # Magic
         dx += ((1 if char[0] < mouse[0] else -1)/10 * abs(char[0] - mouse[0])/distance_divider)/overall_divider
